b2f867c34bd06e528f19e86753a837ca8062bf64
Jitse-Jan van Waterschoot <j.waterschoot@marketinvoice.com>
2018-05-01 15:39:35 +0100

    Update config for The Viji

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,11 +3,11 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'nuxtjs-starter',
+    title: 'The Viji',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { hid: 'description', name: 'description', content: 'The Viji | Home' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
