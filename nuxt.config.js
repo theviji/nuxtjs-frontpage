ac0d0a5a68edd6a7c97175c75fac0709f85d4fcb
Jitse-Jan van Waterschoot <j.waterschoot@marketinvoice.com>
2018-05-01 15:54:44 +0100

    Update font to use Lato

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,8 @@ module.exports = {
       { hid: 'description', name: 'description', content: 'The Viji | Home' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'http://fonts.googleapis.com/css?family=Lato:300,400,700,900' }
     ]
   },
   /*
