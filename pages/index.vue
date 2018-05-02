495f227ae91618193fc7f4d2faffe2720efcc6f4
Jitse-Jan van Waterschoot <j.waterschoot@marketinvoice.com>
2018-05-02 10:34:00 +0100

    Added Github link

--- pages/index.vue
+++ pages/index.vue
@@ -11,6 +11,8 @@
         <a href="https://twitter.com/thisistheviji" target="blank"><i class="ion-social-twitter"></i></a>
         <a href="https://www.instagram.com/thisistheviji" taget="blank"><i class="ion-social-instagram"></i></a>
         <a href="https://www.facebook.com/theviji/" target="blank"><i class="ion-social-facebook"></i></a>
+        <a href="https://github.com/theviji" target="blank"><i class="ion-social-github"></i></a>
+        
       </div>
       <div class="footer">
         Copyright 2016-2018 - The Viji
